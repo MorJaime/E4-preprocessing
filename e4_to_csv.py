5fd5bce06d833debb97a9af3ebd60bbf752fd173
MorJaime <77735808+MorJaime@users.noreply.github.com>
2022-04-25 21:07:02 +0900

    add time zone management

--- e4_to_csv.py
+++ e4_to_csv.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import shutil
 from zipfile import ZipFile
 import pandas as pd
 import numpy as np
@@ -17,16 +18,18 @@ def make_parser():
                         help="list[sens1,sens2,etc.]sensor data to be extracted, {'acc','bvp','eda','temp'}.")
     parser.add_argument('--shift', required=True,
                         help="Milli second to shift csv's timestamp")
+    parser.add_argument('--timezone', default='UTC',
+                        help="Set timezone required for output data. {UTC,Japan}")
     parser.add_argument('--unit', default='g',
                         help="Acc unit to convert to, {g, m/s2}")
     return parser
 
 
-path_to_zip = r'G:/JaimeMorales/Codes/openlogi/20220301_U0201/e4-1/S0200.zip'
-output_dir= r'G:/JaimeMorales/Codes/openlogi/20220301_U0201/e4-1'
-acc_unit = 'g'
-shift = 200
-sensors = []
+#path_to_zip = r'G:/JaimeMorales/Codes/openlogi/20220301_U0201/e4-1/S0200.zip'
+#output_dir= r'G:/JaimeMorales/Codes/openlogi/20220301_U0201/e4-1'
+#acc_unit = 'g'
+#shift = 200
+#sensors = []
 
 def setup_dir(path):
     """ Clean
@@ -42,7 +45,7 @@ def setup_dir(path):
             print(">> Done: create directory [{}]".format(path))
     return path
 
-def load_sensor_data(readir,file,shift=0):
+def load_sensor_data(readir,file,shift=0,tz='UTC'):
     
     path=os.path.join(readir,file)
     df=pd.read_csv(path)
@@ -68,7 +71,13 @@ def load_sensor_data(readir,file,shift=0):
     time_1 = np.array(df['time'])
     time_ms, time = np.modf(time_1)
     df['time'] = time
-    df['time'] = pd.to_datetime(df['time'], yearfirst=True, utc=False, unit='s')
+    if tz=='UTC':
+        utc=True
+    else:
+        utc=False
+    df['time'] = pd.to_datetime(df['time'], yearfirst=True, utc=utc, unit='s')
+    if tz!='UTC':
+        df['time'] = df['time'].dt.tz_localize('UTC').dt.tz_convert(tz)
     df['time_ms'] = np.round(time_ms, 3)
     
     #print(df['time_ms'])
@@ -107,6 +116,7 @@ def save_file(df,output_dir,sensor,sess_id):
     setup_dir(writedir)
     writepath = os.path.join(writedir, sess_id)
     df.to_csv(writepath,index=False)
+    print(writepath)
     
     return
 
@@ -117,25 +127,28 @@ def main():
     
     path_to_zip = str(args.path_to_zip)
 
-    inter_dir = "/".join(path_to_zip.split("/")[:-1])
+    inter_dir = '/'.join(path_to_zip.split('\\')[:-1])
     inter_dir = os.path.join(inter_dir,'interim')
-    sess_id = path_to_zip.split("/")[-1:][0].replace('.zip','.csv')
+    sess_id = path_to_zip.split('\\')[-1:][0].replace('.zip','.csv')
 
     with ZipFile(path_to_zip, 'r') as zipObj:
         setup_dir(inter_dir)
         # Extract all the contents of zip file in different directory
         zipObj.extractall(inter_dir)
         
-    sensors = list[args.sensors]
+    sensors = args.sensors.split(',')
+    print(sensors)
 
     for sensor in sensors:
     
-        df = load_sensor_data(inter_dir,sensor.upper()+'.csv',float(args.shift))
+        df = load_sensor_data(inter_dir,sensor.upper()+'.csv',float(args.shift),tz = str(args.timezone))
     
         if sensor == 'acc':
             df = set_unit(df,str(args.unit))
         
-        save_file(df,str(args.output_dir),sensor,sess_id)
+        save_file(df,str(args.path_output_dir),sensor,sess_id)
+
+    shutil.rmtree(inter_dir)
 
         
 if __name__=='__main__':
